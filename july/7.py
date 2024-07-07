4bff1e72b677a2945c6148f07da7b8565b08aa5b
niteen-singh <viperxoffic@gmail.com>
2024-07-07 08:34:16 +0530

    7-7-24

---
+++ july/7.py
@@ -0,0 +1,29 @@
+#initiation of learning python 
+
+print("Hello, World!")
+
+#we can print anything using print keyword since i am printing an sting so it is quotes
+
+print("Cheers, Mate!")
+
+#Python has no command for declaring a variable.
+#A variable is created the moment you first assign a value to it.
+
+x = 5
+y = "rakesh"
+print(x)
+print(y)
+
+#Variables do not need to be declared with any particular type, and can even change type after they have been set.
+
+# x is of type int
+x = 4       
+# x is now of type str
+x = "suresh" 
+print(x)
+
+#If we want to specify the data type of a variable, this can be done with casting.
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
