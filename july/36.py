ecad31a6b16c7f9f21af85ebcd640b2b82fff083
niteen-singh <viperxoffic@gmail.com>
2024-07-31 10:15:28 +0530

    Functions

---
+++ july/36.py
@@ -0,0 +1,8 @@
+# functions
+def invoice(username, amount, due_date):
+    print(f"Invoice for {username} of amount {amount} due date {due_date}")
+invoice("nitin", 20000, "20-1-2025")
+
+def math(a, b):
+    print(f"addition: {a+b}, subtraction: {a-b}, multiplication: {a*b}, division: {a/b}")
+math(5, 5)
