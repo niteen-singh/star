5d00214cc22a1ea00f887bbfde336346b498f23c
niteen-singh <viperxoffic@gmail.com>
2024-09-01 08:48:01 +0530

    reading files

---
+++ aug/18.py
@@ -0,0 +1,37 @@
+file_path = "output.txt"
+
+try:
+    with open(file_path, 'r') as file:
+        content = file.read()
+        print(content)
+except FileNotFoundError:
+    print("That file was not found")
+except PermissionError:
+    print("You do not have permission to read that file")
+
+import json
+
+file_path = "output.json"
+
+try:
+    with open(file_path, 'r') as file:
+        content = json.load(file)
+        print(content )
+except FileNotFoundError:
+    print("That file was not found")
+except PermissionError:
+    print("You do not have permission to read that file")
+
+import csv
+
+file_path = "output.csv"
+
+try:
+    with open(file_path, 'r') as file:
+        content = csv.reader(file)
+        for line in content:
+            print(line)
+except FileNotFoundError:
+    print("That file was not found")
+except PermissionError:
+    print("You do not have permission to read that file")
